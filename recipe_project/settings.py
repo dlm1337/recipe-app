f20fd39f9c23e79ac6cfc12837b0314cbd02a65e
My Name <dlm1337.dm@gmail.com>
2023-08-07 14:39:43 -0400

    Update settings.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "recipe/static"]
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
